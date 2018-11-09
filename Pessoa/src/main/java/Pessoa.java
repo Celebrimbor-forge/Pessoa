5b462c50e5ef43da93203b09423b4a8eac73774e
Celebrimbor-forge <juansalvador48@gmail.com>
2018-11-09 08:20:18 -0300

    Primeira versão

---
+++ Pessoa/src/main/java/Pessoa.java
@@ -0,0 +1,12 @@
+
+public class Pessoa {
+	private String id;
+	private String password;
+	private String username;
+	private String cidade;
+	private String nome;
+	private String bairro;
+	private String cpf;
+	private String rua;
+
+}
